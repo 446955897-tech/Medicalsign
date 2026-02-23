d727aeb491ee2c1e8e37daddb11b65a65aecd047
leenaDS21 <leenaaldoosari@gmail.com>
2026-02-23 04:34:16 +0300

    final check

--- database/appointments.sql
+++ database/appointments.sql
@@ -14,3 +14,7 @@ CREATE TABLE IF NOT EXISTS appointments (
 DESCRIBE appointments;
 
 SELECT * FROM appointments;
+-- مهمة لينا: ربط الجدول بالمستخدمين
+ALTER TABLE appointments
+ADD CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES users(id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_doctor FOREIGN KEY (doctor_id) REFERENCES users(id) ON DELETE CASCADE;
