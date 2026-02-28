c8406f4be1263a123fad8bd2db1cb4cbf2f29d74
jana <446955897@std.psau.edu.sa>
2026-03-01 01:37:22 +0300

    تحديث users.sql وإضافة بيانات تجريبية

--- database/users.sql
+++ database/users.sql
@@ -1,12 +1,25 @@
 
-CREATE TABLE Users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    role VARCHAR(50)
-);
 
-ALTER TABLE Users
-ADD full_name VARCHAR(150),
-ADD password VARCHAR(255);
+
+USE medicalsign;
+
+ALTER TABLE users
+  ADD COLUMN IF NOT EXISTS  user_id INT,
+  ADD COLUMN IF NOT EXISTS name VARCHAR(100),
+  ADD COLUMN IF NOT EXISTS email VARCHAR(100),
+  ADD COLUMN IF NOT EXISTS password VARCHAR(255),
+  ADD COLUMN IF NOT EXISTS role VARCHAR(50);
+
+ALTER TABLE users
+  MODIFY COLUMN  user_id INT NOT NULL AUTO_INCREMENT,
+  MODIFY COLUMN name VARCHAR(100),
+  MODIFY COLUMN email VARCHAR(100) NOT NULL,
+  MODIFY COLUMN password VARCHAR(255) NOT NULL,
+  MODIFY COLUMN role VARCHAR(50) NOT NULL;
+
+
+
+INSERT INTO users (name, email, password, role) VALUES
+('مريض تجريبي', 'patient@test.com', '1234', 'patient'),
+('طبيب تجريبي', 'doctor@test.com', '1234', 'doctor');
 
