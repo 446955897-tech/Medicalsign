d9a7c25ceb50832c0107d6fba9564ba2d193a19c
Bayann-8080 <446952690@std.psau.edu.sa>
2026-05-03 19:01:15 +0300

    Add language toggle functionality in script.js

--- JAVA S/script.js
+++ JAVA S/script.js
@@ -198,4 +198,50 @@ function saveData() {
 // ==========================================
 // كود إعدادات الحساب - بشاير (نهاية)
 // ==========================================
->>>>>>> 3fc498d9a4eb7c3244740612b68ad89f0d4d0867
+
+
+
+/*-- بداية كود بيان --*/
+function go(key){
+  localStorage.setItem("lang", lang);
+  window.location.href = "medical.html?case=" + key;
+}
+
+// قراءة اللغة المحفوظة
+let lang = localStorage.getItem("lang") || "ar";
+
+// عند فتح الصفحة
+window.onload = function(){
+  applyLang();
+};
+
+function toggleLang(){
+  lang = (lang === "ar") ? "en" : "ar";
+  localStorage.setItem("lang", lang);
+  applyLang();
+}
+
+function applyLang(){
+  let items = document.querySelectorAll("[data-ar]");
+  let btn = document.getElementById("langToggle");
+
+  if(lang === "en"){
+    items.forEach(el => el.innerText = el.dataset.en);
+
+    let title = document.getElementById("mainTitle");
+    if(title) title.innerText = "Medical Sentences";
+
+    if(btn) btn.innerText = "AR";
+    document.documentElement.dir = "ltr";
+
+  } else {
+    items.forEach(el => el.innerText = el.dataset.ar);
+
+    let title = document.getElementById("mainTitle");
+    if(title) title.innerText = "الجمل الطبية";
+
+    if(btn) btn.innerText = "EN";
+    document.documentElement.dir = "rtl";
+  }
+}
+/*-- نهاية كود بيان --*/
